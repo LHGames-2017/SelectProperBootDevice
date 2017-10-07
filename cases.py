43b0e1d9bd4297a948e86967a95345acf07e4701
gunwoofer <valentin.bouis@gmail.com>
2017-10-07 12:06:26 -0400

    VB: cases et itineraire structure

---
+++ cases.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        cases
+# Purpose:
+#
+# Author:      Valentin
+#
+# Created:     07/10/2017
+# Copyright:   (c) Valentin 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+class Cases():
+    casesAccessibles = None
+
+    def __init__(self, cases):
+        casesAccessibles = cases
+
+    def trouverCasesAutour():
+
+
